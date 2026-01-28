c038381d819b0b3a09442516f6f77f7e8f92fb5b
Akhila <davuluriakhila33@gmail.com>
2026-01-28 16:23:15 +0530

    reversefun and isPalindome

---
+++ reverse.py
@@ -0,0 +1,16 @@
+##write a program to find the reverse of a given number
+def reverse(num):
+    rev=0
+    while num>0:
+        rev=rev*10+num%10
+        num//=10
+    return rev
+
+def isPalindrome(num):
+    return num==reverse(num)
+
+print(reverse(123))
+print(ispalindrome(123))
+
+print(reverse(121))
+print(ispalindrome(121))
